82f1d640cc0db5bd2eaffbe707924999eede5e6b
hdjekso <raytamcy@gmail.com>
2025-01-08 22:02:32 -0800

    changed host

--- backend/python/app.py
+++ backend/python/app.py
@@ -270,4 +270,4 @@ def process_audio():
 
 if __name__ == "__main__":
     #FIXME: change host for production
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
